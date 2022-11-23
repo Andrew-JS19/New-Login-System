66bf02a07113b97f7293bc51685726e1230c8fe7
AndrewSamandan <samandanandrew@gmail.com>
2022-11-23 17:41:45 +0200

    added router changes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
+const session =require("express-session")
+const {v4: uuidv4 } = require("uuid");
+
+const router = require('./router');
+
 const app = express();
 
 const port = process.env.PORT||3000;
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.set('view engine','ejs');
 
@@ -10,9 +19,18 @@ app.set('view engine','ejs');
 app.use('/static',express.static(path.join(__dirname,'public')))
 app.use('/assets',express.static(path.join(__dirname,'public/assets')));
 
+app.use(session({
+    secret: uuidv4(),// '1b9d6bcd-bbfd-4b3d-9b5d-ab8dfbbd4bed'
+    resave: false,
+    saveUinitialized:true
+}));
+
+app.use('/route', router);
+
+
 // home route
-app.get('/',(req, res)=>{
-    res.render('base',{title: "Login System"});
+app.get('/',(req, res) => {
+    res.send('base',{title: "Login System"});
 })
 
 app.listen(port, ()=>{console.log("Listening to the server http://localhost:3000")});
